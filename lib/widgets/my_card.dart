ee1c8078b79e7fde2e8449c393782ce676551a7c
Cantika <cantika.kpraja@gmail.com>
2026-01-07 20:38:18 +0700

    card-info_order

--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -5,68 +5,65 @@ class MyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-      child: Container(
-        width: double.maxFinite,
-        height: 155,
-        decoration: BoxDecoration(
-          color: Colors.grey[850],
-
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Row(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Get",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                      const SizedBox(width: 7),
-                      Text(
-                        "40%",
-                        style: TextStyle(color: Colors.red, fontSize: 20),
-                      ),
-                      const SizedBox(width: 7),
-                      Text(
-                        "off when",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    "your order 2 items! 🔥",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  SizedBox(height: 10),
-                  TextButton(
-                    onPressed: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                      child: Text(
-                        "Order now",
-                        style: TextStyle(color: Colors.black, fontSize: 18),
-                      ),
+    return Container(
+      width: double.maxFinite,
+      height: 155,
+      decoration: BoxDecoration(
+        color: Colors.grey[850],
+    
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Get",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
                     ),
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.grey[350],
+                    const SizedBox(width: 7),
+                    Text(
+                      "40%",
+                      style: TextStyle(color: Colors.red, fontSize: 20),
+                    ),
+                    const SizedBox(width: 7),
+                    Text(
+                      "off when",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                ),
+                Text(
+                  "your order 2 items! 🔥",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Text(
+                      "Order now",
+                      style: TextStyle(color: Colors.black, fontSize: 18),
                     ),
                   ),
-                ],
-              ),
-              Expanded(
-                child: Image.asset("lib/assets/kusyiang.jpg", height: 100),
-              ),
-            ],
-          ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.grey[350],
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Image.asset("lib/assets/kusyiang.jpg", height: 100),
+            ),
+          ],
         ),
       ),
     );
