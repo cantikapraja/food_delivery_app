d2b9575689a90f85439a2a803be2deeea29bab61
Cantika <cantika.kpraja@gmail.com>
2026-01-07 20:37:53 +0700

    food-list

---
+++ lib/models/my_list_model.dart
@@ -0,0 +1,12 @@
+class MyListModel {
+  final String name;
+  MyListModel({required this.name});
+}
+
+List<MyListModel> myList = [
+  MyListModel(name: "All"),
+  MyListModel(name: "Burger"),
+  MyListModel(name: "Sticks"),
+  MyListModel(name: "Pizza"),
+  MyListModel(name: "Sandwich"),
+];
