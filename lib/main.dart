18854d64bbad24bc222ab0d88a7186b78e1d59b5
Cantika <cantika.kpraja@gmail.com>
2026-01-07 21:37:50 +0700

    main-code

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: Colors.black,
+        ),
+      ),
+    );
   }
 }
