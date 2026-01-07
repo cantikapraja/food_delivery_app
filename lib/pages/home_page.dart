a39f169d107a75d3f5154de87c3ffa1c7b5ca8ab
Cantika <cantika.kpraja@gmail.com>
2026-01-07 20:38:02 +0700

    home-page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,32 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/widgets/my_list.dart';
 import 'package:my_app/widgets/my_text_field.dart';
 
 import '../widgets/my_card.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[150],
       body: SafeArea(
         child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // APP BAR
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 10,
-                ),
-                child: Row(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // APP BAR
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Icon(Icons.menu_rounded, size: 25),
@@ -45,29 +38,28 @@ class _HomePageState extends State<HomePage> {
                     Icon(Icons.notifications_none_rounded, size: 30),
                   ],
                 ),
-              ),
-              const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-              // CARD ORDER NOW
-              MyCard(),
-              const SizedBox(height: 25),
+                // CARD ORDER NOW
+                MyCard(),
+                const SizedBox(height: 25),
 
-              //SEARCH BAR
-              MyTextField(),
-              const SizedBox(height: 25),
+                //SEARCH BAR
+                MyTextField(),
+                const SizedBox(height: 25),
 
-              // CATEGORIES LIST
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: Text(
+                // CATEGORIES LIST
+                Text(
                   "Categories",
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                 ),
-              ),
-              const SizedBox(height: 10),
-              
-              //CATEGORIES SECTION
-            ],
+                MyList(),
+                const SizedBox(height: 10),
+
+                //CATEGORIES SECTION
+                
+              ],
+            ),
           ),
         ),
       ),
